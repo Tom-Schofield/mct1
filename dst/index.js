a9e23a85cfb0f4de8a0d3bb844ba3935a3a0c44e
Thomas Schofield <thomas.schofield@uq.net.au>
2017-07-23 11:31:59 +1000

    Build

--- dst/index.js
+++ dst/index.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-magikcraft.io.dixit('MCT1 loading...');
 var log_1 = require("./util/log");
 var BGLBarGlucoseMonitor_1 = require("./GlucoseMonitor/BGLBarGlucoseMonitor/BGLBarGlucoseMonitor");
 var mct1_1 = require("./util/mct1");
 var T1Player_1 = require("./Player/T1Player");
 mct1_1.mct1.version = '1.3.0';
 log_1.log("MCT1 version " + mct1_1.mct1.version);
+log_1.log("This is a test...");
 function _default() {
     if (!mct1_1.mct1.initialised) {
         initialise();
