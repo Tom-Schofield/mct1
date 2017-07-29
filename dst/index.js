19187f38023d9d9a123ad2db5f557a5ba4a91614
Thomas Schofield <thomas.schofield@uq.net.au>
2017-07-29 18:44:49 +1000

    Hopefully fix set interval issues

--- dst/index.js
+++ dst/index.js
@@ -8,6 +8,7 @@ var T1Player_1 = require("./Player/T1Player");
 var Carbohydrate_1 = require("./Carbs/Carbohydrate");
 var Insulin_1 = require("./Insulin/Insulin");
 var magikcraft_lore_ui_bar_1 = require("magikcraft-lore-ui-bar");
+var timer_1 = require("./util/timer");
 mct1_1.mct1.version = '1.3.0';
 log_1.log("MCT1 version " + mct1_1.mct1.version);
 log_1.log("This is a test");
@@ -103,5 +104,5 @@ function initialise(callback) {
     callback && callback();
     var bar = createBGLBar(player.BGL.getBGLmmolL());
     // Set the BGL bar to periodically update every 200ms
-    setInterval(updateBar, 6000, bar, player);
+    timer_1.Interval.setInterval(updateBar, 6000, bar, player);
 }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { T1Player } from './Player/T1Player';
 import { Carbohydrate } from './Carbs/Carbohydrate';
 import { Insulin } from './Insulin/Insulin';
 import { bar, color, style } from 'magikcraft-lore-ui-bar';
-
+import {Interval} from './util/timer'
 mct1.version = '1.3.0';
 log(`MCT1 version ${mct1.version}`);
 log(`This is a test`);
@@ -123,7 +123,6 @@ function initialise(callback?: () => void) {
     callback && callback();
 
     var bar = createBGLBar(player.BGL.getBGLmmolL());
-
     // Set the BGL bar to periodically update every 200ms
-    setInterval(updateBar, 6000, bar, player);
+    Interval.setInterval(updateBar, 6000, bar, player);
 }   
